38b83e4daac0bd8d17a9615bbd6b5134f45db25e
arum <arumshin.dev@gmail.com>
2024-02-18 20:07:37 +0900

    model username 변수 넘기기

--- src/main/java/com/example/springBoot322Java17GradleGroovy/controller/FirstController.java
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/controller/FirstController.java
@@ -1,13 +1,16 @@
 package com.example.springBoot322Java17GradleGroovy.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hi")
-    public String niceToMeetYou(){
+    public String niceToMeetYou(Model model){
+        model.addAttribute("userName","arum");
         return "greetings";//templates/greetings.mistache
     }
 }

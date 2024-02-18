2f72726b1d525257bacf30cd167e437451cb0cd8
arum <arumshin.dev@gmail.com>
2024-02-18 19:14:40 +0900

    spring boot 3.2.2 java 17 gradle(groovy added dependencies(Spring Web, H2 Database, Mustache, Spring Data JPA)

---
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/SpringBoot322Java17GradleGroovyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBoot322Java17GradleGroovy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot322Java17GradleGroovyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot322Java17GradleGroovyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springBoot322Java17GradleGroovy/SpringBoot322Java17GradleGroovyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBoot322Java17GradleGroovy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot322Java17GradleGroovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

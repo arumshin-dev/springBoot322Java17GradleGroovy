1de23652f0ae22ce5ad00c3593daa0dad4e0eea7
arum <arumshin.dev@gmail.com>
2024-02-27 17:45:30 +0900

    build.gradle 롬복 추가 gradle build 확인 help->fild action :plugins 검색 marketplace 가서 lombok install file->settings->Build, Excution, Deployment->compilr->Annotation Processors : Emable annotatio processing 체크 ok ArticleForm.java 생성자 @AllArgsConstructor, toString() @ToString 변경 Article.java 생성자 @AllArgsConstructor, toString() @ToString 변경 ArticleController.java @Slf4j 로깅 추가 System.out.println()->log.info()로 대체

--- src/main/java/com/example/springBoot322Java17GradleGroovy/entity/Article.java
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/entity/Article.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity//DB가 해당 객체를 인식 가능
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Article {
     
     @Id//대표값 지정 PK
@@ -18,7 +24,7 @@ public class Article {
     @Column
     private String content;
 
-    //생성자 생성
+    /*//생성자 생성
     public Article(Long id, String title, String content) {
         this.id = id;
         this.title = title;
@@ -32,5 +38,5 @@ public class Article {
                 ", title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
-    }
+    }*/
 }

771ef1dd321f264f60d4d61c6b9d4f68fa2444ad
arum <arumshin.dev@gmail.com>
2024-03-21 21:55:02 +0900

    댓글 서비스와 컨트롤러 @RestController

---
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/dto/CommentDto.java
@@ -0,0 +1,26 @@
+package com.example.springBoot322Java17GradleGroovy.dto;
+
+import com.example.springBoot322Java17GradleGroovy.entity.Comment;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class CommentDto {
+    private Long id;
+    @JsonProperty("article_id")
+    private Long articleId;
+    private String nickname;
+    private String body;
+
+    public static CommentDto createCommentDto(Comment comment) {
+        return new CommentDto(
+                comment.getId(),
+                comment.getArticle().getId(),
+                comment.getNickname(),
+                comment.getBody()
+        );
+    }
+}

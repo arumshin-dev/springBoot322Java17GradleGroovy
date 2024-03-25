7ac86edb7c11a13a3998f0860e8c2ad62324779b
arum <arumshin.dev@gmail.com>
2024-03-25 20:18:03 +0900

    돈가스 요리하기 테스트

---
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/ioc/Pork.java
@@ -0,0 +1,11 @@
+package com.example.springBoot322Java17GradleGroovy.ioc;
+
+public class Pork {
+    private String name;
+    public Pork(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}

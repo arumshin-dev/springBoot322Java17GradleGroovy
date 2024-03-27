6b3432b04c100c1a2db905fb06e16f90f4a6b3d5
arum <arumshin.dev@gmail.com>
2024-03-27 11:24:29 +0900

    JsonNode 활용하기 - readTree(), JsonNode, toPrettyString()

--- src/test/java/com/example/springBoot322Java17GradleGroovy/objectmapper/BurgerTest.java
+++ src/test/java/com/example/springBoot322Java17GradleGroovy/objectmapper/BurgerTest.java
@@ -1,6 +1,7 @@
 package com.example.springBoot322Java17GradleGroovy.objectmapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
@@ -23,6 +24,7 @@ public class BurgerTest {
         String expected = "{\"name\":\"맥도날드 슈비버거\",\"price\":5500,\"ingredients\":[\"통새우 패티\",\"순쇠고기 패티\",\"토마토\",\"스파이시 어니언 소스\"]}";
         // 검증
         assertEquals(expected, json);
-        System.out.println(json);
+        JsonNode jsonNode = objectMapper.readTree(json);
+        System.out.println(jsonNode.toPrettyString());
     }
 }

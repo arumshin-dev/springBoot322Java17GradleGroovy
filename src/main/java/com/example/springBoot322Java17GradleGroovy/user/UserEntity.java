b935b2aca7f02917851f957c1f904d49601a45a9
arum <arumshin.dev@gmail.com>
2024-08-30 22:56:48 +0900

    postgresql user 테이블 생성

---
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserEntity.java
@@ -0,0 +1,55 @@
+package com.example.springBoot322Java17GradleGroovy.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    //vo dto
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_sq")
+    private long userSq;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    public long getUserSq() {
+        return userSq;
+    }
+
+    public void setUserSq(long userSq) {
+        this.userSq = userSq;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

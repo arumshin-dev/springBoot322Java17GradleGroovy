11faab65e7c277e0721aee43c6e432f6f32e2b8a
arum <arumshin.dev@gmail.com>
2024-08-31 21:48:51 +0900

    패스워드 단방향 암호화 설정(SHA-512)

--- src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserService.java
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.springBoot322Java17GradleGroovy.user;
 
+import com.example.springBoot322Java17GradleGroovy.util.HashUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,11 @@ public class UserService {
     private UserRepository userRepository;
 
     public UserEntity login(String userId, String password) {
+
+        String encPassword = HashUtil.hash(password);//암호화 패스워드 SHA-512
+        System.out.println(encPassword);
         UserEntity userEntity = new UserEntity();
-        Optional<UserEntity> optionalUserEntity = userRepository.findByUserIdAndPassword(userId, password);//쿼리 호출
+        Optional<UserEntity> optionalUserEntity = userRepository.findByUserIdAndPassword(userId, encPassword);//쿼리 호출
         if (optionalUserEntity.isPresent()) {
             userEntity = optionalUserEntity.get();
             return userEntity;
---
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/util/HashUtil.java
@@ -0,0 +1,19 @@
+package com.example.springBoot322Java17GradleGroovy.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class HashUtil {
+
+    public static String hash(String value) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] hash = md.digest(value.getBytes());
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException ex) {
+            return null;
+        }
+    }
+}
+

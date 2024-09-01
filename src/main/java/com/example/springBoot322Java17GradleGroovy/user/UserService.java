8ffe49dff31bf07472a29af0c8dc50bab512eb1b
arum <arumshin.dev@gmail.com>
2024-09-01 15:41:48 +0900

    로그인 오류 처리

--- src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserService.java
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserService.java
@@ -4,6 +4,8 @@ import com.example.springBoot322Java17GradleGroovy.util.HashUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -11,24 +13,33 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public UserEntity login(String userId, String password) {
-
-//        String encPassword = HashUtil.hash(password);//암호화 패스워드 SHA-512
+    public Map<String, Object> login(String userId, String password) {
+        Map<String, Object> resultMap = new HashMap<>();
+//        String encPassword = HashUtil.hash(password);//암호화 패스워드SHA-512
 //        System.out.println(encPassword);
 //        String saltPassword = HashUtil.hashWithGeneratedSalt(password);
-//        System.out.println(saltPassword);//+salt
+//        System.out.println(saltPassword);
         UserEntity userEntity = new UserEntity();
-        Optional<UserEntity> optionalUserEntity = userRepository.findByUserId(userId);//쿼리 호출
-        if (optionalUserEntity.isPresent()) {
+        Optional<UserEntity> optionalUserEntity = userRepository.findByUserId(userId);
+        if(optionalUserEntity.isPresent()) {
             userEntity = optionalUserEntity.get();
             boolean pwChk = HashUtil.verifyHash(password, userEntity.getPassword());
-            if (pwChk) {//로그인 성공
-                return userEntity;
-            }else {//비밀번호가 일치하지 않음
-                return null;
+            if(pwChk) {//로그인 성공
+                resultMap.put("success", true);
+                resultMap.put("message", "로그인에 성공하였습니다.");
+                resultMap.put("data", userEntity);
+                return resultMap;
+            }else{//비밀번호가 일치하지 않음
+                resultMap.put("success", false);
+                resultMap.put("message", "비밀번호가 일치하지 않습니다.");
+                resultMap.put("data", "");
+                return resultMap;
             }
-        }else {//일치하는 사용자 없음
-            return null;
+        }else{//일치하는 사용자가 없음
+            resultMap.put("success", false);
+            resultMap.put("message", "해당하는 사용자가 없습니다.");
+            resultMap.put("data", "");
+            return resultMap;
         }
     }
 }

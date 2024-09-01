1ea34efa1295e1edefc73c6ca8eabfea30c27e7f
arum <arumshin.dev@gmail.com>
2024-09-01 16:19:11 +0900

    로그인 입력값 숨김 결과값 비밀번호 숨김

--- src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserController.java
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserController.java
@@ -3,10 +3,7 @@ package com.example.springBoot322Java17GradleGroovy.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,18 +16,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<Map<String, Object>> login(
-            @RequestParam("userId") String userId,
-            @RequestParam("password") String password
+            @RequestBody LoginRequest loginRequest
     ) {
         Map<String, Object> resultMap = new HashMap<>();
+        String userId = loginRequest.getUserId();
         if(userId == null) {
             resultMap.put("success", false);
             resultMap.put("message", "사용자ID를 입력해주세요.");
             resultMap.put("data", "");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
         }
+        String password = loginRequest.getPassword();
         if(password == null) {
             resultMap.put("success", false);
             resultMap.put("message", "비밀번호를 입력해주세요.");

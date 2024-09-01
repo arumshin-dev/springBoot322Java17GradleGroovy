96cab981351b20e956f94e6dd21ed38b7b4a0431
arum <arumshin.dev@gmail.com>
2024-09-01 15:43:46 +0900

    로그인 null check

--- src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserController.java
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserController.java
@@ -1,10 +1,16 @@
 package com.example.springBoot322Java17GradleGroovy.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -14,8 +20,25 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/login")
-    public UserEntity login(@RequestParam("userId") String userId, @RequestParam("password") String password) {
-        return userService.login(userId, password);
+    public ResponseEntity<Map<String, Object>> login(
+            @RequestParam("userId") String userId,
+            @RequestParam("password") String password
+    ) {
+        Map<String, Object> resultMap = new HashMap<>();
+        if(userId == null) {
+            resultMap.put("success", false);
+            resultMap.put("message", "사용자ID를 입력해주세요.");
+            resultMap.put("data", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
+        }
+        if(password == null) {
+            resultMap.put("success", false);
+            resultMap.put("message", "비밀번호를 입력해주세요.");
+            resultMap.put("data", "");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resultMap);
+        }
+        resultMap = userService.login(userId, password);
+        return ResponseEntity.ok(resultMap);
     }
 
 }

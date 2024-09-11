e3a086db27d77b5f28c484bf52cadb3a76386122
arum <arumshin.dev@gmail.com>
2024-09-11 20:29:22 +0900

    pageTitle 수정

--- src/main/java/com/example/springBoot322Java17GradleGroovy/router/login/LoginRouter.java
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/router/login/LoginRouter.java
@@ -1,18 +1,21 @@
 package com.example.springBoot322Java17GradleGroovy.router.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginRouter {
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("pageTitle","Login Page");
         return "login";
     }
 
     @GetMapping("/join")
-    public String join() {
+    public String join(Model model) {
+        model.addAttribute("pageTitle","Join Page");
         return "join";
     }
 }

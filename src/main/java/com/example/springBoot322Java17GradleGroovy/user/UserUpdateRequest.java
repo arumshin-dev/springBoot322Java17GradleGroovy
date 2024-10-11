a37903d413ff74123ad2817bad4897c400542434
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-10-11 23:28:35 +0900

    회원정보 수정 기능 개발

---
+++ src/main/java/com/example/springBoot322Java17GradleGroovy/user/UserUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.example.springBoot322Java17GradleGroovy.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateRequest {
+
+    private String email;
+
+    private String tel;
+}
